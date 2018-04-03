ae8d49cc4c732730a280d74752593b27218da5e7
jsbecerrab <36463265+jsbecerrab@users.noreply.github.com>
2018-04-03 11:50:37 -0500

    Update shell2.c

--- Chap3_Processes/Project1_UNIX_Shell_and_History_Feature/shell2.c
+++ Chap3_Processes/Project1_UNIX_Shell_and_History_Feature/shell2.c
@@ -5,6 +5,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #define MAX_LINE 80 /* The maximum length of a command */
 #define BUFFER_SIZE 50
